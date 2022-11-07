3b0bad390483fe23848aa66b650d98c7ab6da189
Dean Troyer <dt@xr7.org>
2022-11-06 21:50:51 -0600

    Add additional test cases for DecodePacketBuffer
    Signed-off-by: Dean Troyer <dt@xr7.org>

--- flight/message_test.go
+++ flight/message_test.go
@@ -12,6 +12,8 @@ import (
 	"gotest.tools/v3/assert"
 )
 
+// Test case 1: original example
+
 const testJson1 = `{
   "header": "QUlS",
   "tail_number": "N20904",
@@ -23,32 +25,85 @@ const testJson1 = `{
   "temperature": -53.2
 }`
 
-var testPkt1 = []byte{
+var testPacket1 = []byte{
 	0x41, 0x49, 0x52, 0x00, 0x00, 0x00, 0x06, 0x4E, 0x32, 0x30, 0x39, 0x30, 0x34, 0x00, 0x00, 0x00,
 	0x02, 0x00, 0x00, 0x00, 0x07, 0x47, 0x45, 0x6E, 0x78, 0x2D, 0x31, 0x42, 0x40, 0x43, 0x8E, 0xD6,
 	0xEB, 0xFF, 0x60, 0x1D, 0xC0, 0x50, 0xD4, 0xC0, 0x91, 0x63, 0x01, 0x65, 0x40, 0xE2, 0x03, 0xF0,
 	0x00, 0x00, 0x00, 0x00, 0xC0, 0x4A, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9A,
 }
 
-func TestReadPktBuf(t *testing.T) {
-	var expect FlightMessage
-	err := json.Unmarshal([]byte(testJson1), &expect)
-	// fmt.Printf("tn: %s\n", expect.TailNumber)
-	if err != nil {
-		fmt.Println("json error: ", err)
-	}
-	buf := FlightMessage{}
+// Test case 2: extended fields
 
-	err = DecodePacketBuffer(bytes.NewBuffer(testPkt1), &buf)
-	if err != nil {
-		fmt.Println("error: ", err)
+const testJson2 = `{
+  "header": "QUlS",
+  "tail_number": "N2090423",
+  "engine_count": 3,
+  "engine_name": "CF6-80C2D1F",
+  "latitude": 39.11593389482025,
+  "longitude": -67.32425341289998,
+  "altitude": 36895.5,
+  "temperature": -53.2
+}`
+
+var testPacket2 = []byte{
+	0x41, 0x49, 0x52, 0x00, 0x00, 0x00, 0x08, 0x4E, 0x32, 0x30, 0x39, 0x30, 0x34, 0x32, 0x33, 0x00,
+	0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x0b, 0x43, 0x46, 0x36, 0x2D, 0x38, 0x30, 0x43, 0x32, 0x44,
+	0x31, 0x46, 0x40, 0x43, 0x8E, 0xD6, 0xEB, 0xFF, 0x60, 0x1D, 0xC0, 0x50, 0xD4, 0xC0, 0x91, 0x63,
+	0x01, 0x65, 0x40, 0xE2, 0x03, 0xF0, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x4A, 0x99, 0x99, 0x99, 0x99,
+	0x99, 0x9A,
+}
+
+// Test case 3: null fields
+
+const testJson3 = `{
+  "header": "QUlS",
+  "tail_number": "",
+  "engine_count": 0,
+  "engine_name": "",
+  "latitude": 0,
+  "longitude": -67.32425341289998,
+  "altitude": 36895.5,
+  "temperature": -53.2
+}`
+
+var testPacket3 = []byte{
+	0x41, 0x49, 0x52, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x50, 0xD4, 0xC0, 0x91, 0x63, 0x01, 0x65, 0x40,
+	0xE2, 0x03, 0xF0, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x4A, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9A,
+}
+
+type CaseTable struct {
+	expected string
+	bindata  []byte
+}
+
+func TestDecodePacketBuffer(t *testing.T) {
+	data := []CaseTable{
+		{testJson1, testPacket1},
+		{testJson2, testPacket2},
+		{testJson3, testPacket3},
 	}
-	assert.Equal(t, FlightMessageHeader, buf.Header)
-	assert.Equal(t, "N20904", buf.TailNumber)
-	assert.Equal(t, uint32(2), buf.EngineCount)
-	assert.Equal(t, "GEnx-1B", buf.EngineName)
-	assert.Equal(t, 39.11593389482025, buf.Latitude)
-	assert.Equal(t, -67.32425341289998, buf.Longitude)
-	assert.Equal(t, 36895.5, buf.Altitude)
-	assert.Equal(t, -53.2, buf.Temperature)
+
+	for _, c := range data {
+		var expect FlightMessage
+		err := json.Unmarshal([]byte(c.expected), &expect)
+		fmt.Printf("tn: %s\n", expect.TailNumber)
+		if err != nil {
+			fmt.Println("json error: ", err)
+		}
+		buf := FlightMessage{}
+
+		err = DecodePacketBuffer(bytes.NewBuffer(c.bindata), &buf)
+		if err != nil {
+			fmt.Println("error: ", err)
+		}
+		assert.Equal(t, FlightMessageHeader, buf.Header)
+		assert.Equal(t, expect.TailNumber, buf.TailNumber)
+		assert.Equal(t, expect.EngineCount, buf.EngineCount)
+		assert.Equal(t, expect.EngineName, buf.EngineName)
+		assert.Equal(t, expect.Latitude, buf.Latitude)
+		assert.Equal(t, expect.Longitude, buf.Longitude)
+		assert.Equal(t, expect.Altitude, buf.Altitude)
+		assert.Equal(t, expect.Temperature, buf.Temperature)
+	}
 }
